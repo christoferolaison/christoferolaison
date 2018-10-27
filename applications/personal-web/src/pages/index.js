a1c42858a71344fa0a7a797caf8777621800ada0
Christofer Olaison <christofer.olaison@mentimeter.com>
2018-10-27 22:34:39 +0200

    chore: gatsby

---
+++ applications/personal-web/src/pages/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Text from '@christoferolaison/text'
+
+export default () => <Text>Hello world!</Text>
---
+++ primitives/Text/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Text({ children }) {
+  return <span style={{ color: 'pink' }}>{children}</span>
+}
+
+export default Text
