a38641bdd3983e1d8dfc864d14012a39a2db6bdf
Christofer Olaison <christofer.olaison@mentimeter.com>
2018-10-28 01:17:30 +0200

    chore: prettier

--- applications/personal-web/src/pages/index.js
+++ applications/personal-web/src/pages/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
-// import Text from '@christoferolaison/text'
+import Text from '@christoferolaison/text'
 
-export default () => <p>Hello world!</p>
+export default () => <Text>Hello world!</Text>
