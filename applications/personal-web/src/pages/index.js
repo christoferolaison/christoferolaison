7ef2a20520cd52ca2bff019de287d7728c3471cf
Christofer Olaison <christofer.olaison@mentimeter.com>
2018-10-28 12:59:04 +0100

    feat(personal-web): about

--- applications/personal-web/src/pages/index.js
+++ applications/personal-web/src/pages/index.js
@@ -1,4 +1,11 @@
 import React from 'react'
 import { Text } from '@christoferolaison/primitives'
 
-export default () => <Text>Hej!</Text>
+export default () => (
+  <>
+    <Text size={18} weight="bold">
+      Hej!
+    </Text>
+    <Text>JavaScript Developer</Text>
+  </>
+)
