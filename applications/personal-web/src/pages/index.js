8b3d0aa6bea2080357043dd580eb45a7e9ee885e
Christofer Olaison <christofer.olaison@mentimeter.com>
2018-10-28 17:05:32 +0100

    fix(personal-web): size

--- applications/personal-web/src/pages/index.js
+++ applications/personal-web/src/pages/index.js
@@ -4,7 +4,7 @@ import { Text } from '@christoferolaison/primitives'
 export default () => (
   <>
     <Text size={100} weight="bold" align="center">
-      Hej!
+      Hej hej tja!
     </Text>
     <Text align="center">JavaScript Developer</Text>
   </>
